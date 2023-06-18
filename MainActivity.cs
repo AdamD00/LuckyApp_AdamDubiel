a7d6a1d61036674ee6bae9009ebe6cf01339c3cf
Mrokon <mrokon00@gmail.com>
2023-06-18 08:39:04 +0200

    All App

---
+++ MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        SeekBar seekBar;
+        TextView resultTextView;
+        Button rollButton;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            seekBar = FindViewById<SeekBar>(Resource.Id.seekBar1);
+            resultTextView = FindViewById<TextView>(Resource.Id.resultTextView);
+            resultTextView.Text = "5";
+            rollButton = FindViewById<Button>(Resource.Id.button1);
+            rollButton.Click += RollButton_Click;
+
+            SupportActionBar.Title = "My Lucky Number";
+        }
+
+        private void RollButton_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int luckyNumber = random.Next(seekBar.Progress) + 1;
+            resultTextView.Text = luckyNumber.ToString();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
